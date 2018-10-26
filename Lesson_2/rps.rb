e76bc912e3b454cc429eb85b902f5945a10a6ce3
Melissa Brown <melissabrown@gmail.com>
2018-10-26 14:47:44 +1030

    Rubocop

--- Lesson_2/rps.rb
+++ Lesson_2/rps.rb
@@ -1,5 +1,3 @@
-require 'pry'
-
 class Player
   attr_accessor :move, :name, :score, :history, :weights
 
@@ -15,7 +13,7 @@ class History
   attr_accessor :move_history
 
   def initialize
-     @move_history = []
+    @move_history = []
   end
 
   def update_history(move)
@@ -30,19 +28,17 @@ end
 class Move
   VALUES = ['rock', 'paper', 'scissors']
 
-  WINNING_MOVES = { 'rock' => ['scissors','lizard'],
-                  'paper'=> ['rock','spock'],
-                  'scissors' => ['paper','lizard'],
-                  'spock' => ['rock','scissors'],
-                  'lizard' => ['spock','paper']
-                  }
+  WINNING_MOVES = { 'rock' => ['scissors', 'lizard'],
+                    'paper' => ['rock', 'spock'],
+                    'scissors' => ['paper', 'lizard'],
+                    'spock' => ['rock', 'scissors'],
+                    'lizard' => ['spock', 'paper'] }
 
-  LOSING_MOVES = { 'rock'=> ['paper','spock'],
-                  'paper' =>['scissors','lizard'],
-                  'scissors'=> ['rock','spock'],
-                  'spock' => ['paper', 'lizard'],
-                  'lizard' => ['scissors', 'spock']
-                 }
+  LOSING_MOVES = { 'rock' => ['paper', 'spock'],
+                   'paper' => ['scissors', 'lizard'],
+                   'scissors' => ['rock', 'spock'],
+                   'spock' => ['paper', 'lizard'],
+                   'lizard' => ['scissors', 'spock'] }
 
   def initialize(value)
     @value = value
@@ -73,13 +69,6 @@ class Move
   end
 end
 
-class Rule
-  def initialize
-    # not sure what the "state" of a rule object should be
-  end
-end
-
-# not sure where "compare" goes yet
 def compare(move1, move2)end
 
 class Human < Player
@@ -91,7 +80,7 @@ class Human < Player
       break unless n.empty?
       puts "Sorry, you must enter a name."
     end
-    self.name = n # can i use @name here?
+    self.name = n
   end
 
   def choose
@@ -139,26 +128,6 @@ class RPSGame
     puts "#{computer.name} chose: #{computer.move}"
   end
 
-  def determine_round_winner
-    winner = nil
-    if human.move > computer.move
-      winner = human.name
-    elsif human.move < computer.move
-      winner = computer.name
-    else
-      nil
-    end
-    winner
-  end
-
-  def display_winner
-    if determine_round_winner == nil
-      puts "It's a tie"
-    else
-      puts "#{determine_round_winner.to_s} won this round!"
-    end
-  end
-
   def increment_score
     if human.move > computer.move
       human.score += 1
@@ -185,28 +154,32 @@ class RPSGame
     return true if answer.downcase == 'y'
   end
 
+  def display_winner
+    if human.move > computer.move
+      human.name
+    elsif human.move < computer.move
+      computer.name
+    else
+      "It's a tie "
+    end
+  end
+
   def display_final_winner
-     winner = nil
-     if human.score > computer.score
-       winner = human.name
-     elsif computer.score > human.score
-       winner = computer.name
-     else
-       winner = "Its a tie"
-     end
+    if human.score > computer.score
+      human.name
+    elsif computer.score > human.score
+      computer.name
+    else
+      "Its a tie"
+    end
   end
 
   def display_final_score
-    if game_over?
-      puts "#{display_final_winner} is the WINNER"
-    end
+    return puts "#{display_final_winner} is the WINNER" if game_over?
   end
 
   def reset_score
-    if game_over?
-      human.score = 0
-      computer.score = 0
-    end
+    return human.score = 0 && computer.score = 0 if game_over?
   end
 
   def game_over?
@@ -214,18 +187,16 @@ class RPSGame
   end
 
   def update_history
-     human.history.update_history(human.move)
-     computer.history.update_history(computer.move)
+    human.history.update_history(human.move)
+    computer.history.update_history(computer.move)
   end
 
   def display_history
-     puts "------------------------------------"
-     puts "#{human.name}'s History of moves is:"
-     puts " #{human.history.display}"
-     puts "- - - - - - - - - - - - - - - - - - - "
-     puts "#{computer.name}'s History of moves is"
-     puts "#{computer.history.display}"
-     puts "------------------------------------"
+    puts "#{human.name}'s History of moves is:"
+    puts human.history.display.to_s
+    puts "- - - - - - - - - - - - - - - - - - - "
+    puts "#{computer.name}'s History of moves is"
+    puts computer.history.display.to_s
   end
 
   def show_history
@@ -239,7 +210,7 @@ class RPSGame
       end
     end
 
-    answer.downcase == 'y' ? display_history :  false
+    answer.downcase == 'y' ? display_history : false
   end
 
   def play
